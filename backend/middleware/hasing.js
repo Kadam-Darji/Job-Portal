44e6e4951255051a7dbb3b9a7b03f185d43e2b3c
Kadam-Darji <81846811+Kadam-Darji@users.noreply.github.com>
2023-07-24 17:21:16 +0530

    Delete backend directory

--- backend/middleware/hasing.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcrypt');
-
-module.exports.encryption = async function encryption(req,res,next){
-    let salt = await bcrypt.genSalt();
-    let hasedString = await bcrypt.hash(req.body.Password,salt);
-    req.body.Password = hasedString;
-    next();
-}
